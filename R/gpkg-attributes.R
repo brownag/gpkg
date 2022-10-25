ebb6f8ac28bf0a9d6e648e6e6dcbd7b4abb280b3
Andrew Brown <andrew.g.brown@usda.gov>
2022-10-24 23:38:47 -0700

    Add gpkg_remove_attributes() and export add/delete gpkg_*_contents() methods
     - gpkg_add_contents: Allow custom extent and srsid, create table if missing

--- R/gpkg-attributes.R
+++ R/gpkg-attributes.R
@@ -1,6 +1,6 @@
-#' Write an attribute table to GeoPackage
+#' Write or Remove Attribute Table in a GeoPackage
 #' 
-#' Specify a target geopackage, data.frame of input data, and name for table.
+#' `gpkg_write_attributes()`: Specify a target geopackage and name for new table. For adding attributes, specify the new data as data.frame. The table name will be registered in the `gpkg_contents` table. Optionally include a description and/or a template terra object that can be used to define the extent of the attribute data.
 #'
 #' @param x a geopackage object
 #' @param table a data.frame
@@ -9,7 +9,7 @@
 #' @param template a list (containing elements `"ext"` and `"crs"`, or a terra object. These are defining xmin/ymin/xmax/ymax and CRS of the attribute table.
 #' @param overwrite overwrite? Default `FALSE`
 #' @param append append? Default `FALSE`
-#'
+#' @rdname gpkg-attributes
 #' @export
 gpkg_write_attributes <-
   function(x,
@@ -32,5 +32,30 @@ gpkg_write_attributes <-
   if (attr(con, 'disconnect')) {
     gpkg_disconnect(x)
   }
+  x
+}
+
+#' @description `gpkg_remove_attributes()`: Remove an attribute table and corresponding `gpkg_contents` record
+#' @rdname gpkg-attributes
+#' @export
+gpkg_remove_attributes <- function(x, table_name) {
+  
+  con <- .gpkg_connection_from_x(x)
+  
+  # write new table
+  if (!is.null(con)) {
+    for (y in table_name) {
+      RSQLite::dbRemoveTable(con, y)
+      if (y %in% gpkg_contents(x)$table_name) {
+        gpkg_delete_contents(x, y)
+      }
+    }
+  }
+  
+  # close connection if needed
+  if (attr(con, 'disconnect')) {
+    gpkg_disconnect(x)
+  }
+  
   x
 }
--- R/gpkg-contents.R
+++ R/gpkg-contents.R
@@ -1,28 +1,108 @@
-#' Build `gpkg_contents` table based on existing tables
+
+#' Add, Remove, Update and Create `gpkg_contents` table and records
+#' @description `gpkg_add_contents()`: Add a record to `gpkg_contents`
+#' @param x A _geopackage_
+#' @param table_name Name of table to add or remove record for in _gpkg_contents_
+#' @param description Default `""`
+#' @param template Default `NULL` uses global EPSG:4326 with bounds -180,-90:180,90
 #'
-#' @param x a _geopackage_ object
-#'
-#' @return logical. `TRUE` on successful update (all non-standard tables added to gpkg_contents table)
+#' @return A _geopackage_
+#' @rdname gpkg-contents
+#' @export
+gpkg_add_contents <- function(x, table_name, description = "", template = NULL) {
+  
+  if (!missing(template) && !is.null(template)) {
+    # template as a list
+    if (is.list(template) && all(c("ext", "srsid") %in% names(template))) {
+      ex <- template$ext
+      cr <- as.integer(template$srsid)
+    } else {
+      ## TODO: calculate ext from object, calculate srsid from WKT (?)
+      # if (!requireNamespace("terra")) {
+      #   stop("package `terra` is required to add contents with a custom extent", call. = FALSE)
+      # }
+      # 
+      # # convert sf object to SpatVector
+      # if (inherits(template, 'sf')) {
+      #   template <- terra::vect(template)
+      # }
+      # 
+      # # template as terra object
+      # if (inherits(template, c("SpatRaster", "SpatVector", "SpatVectorProxy"))){
+      #   ex <- as.numeric(terra::ext(template))
+      #   cr <- as.character(terra::crs(template))
+      # }
+    }
+  } else {
+    ex <- c(-180, -90, 180, 90)
+    cr <- 4326
+  }
+  
+  # create gpkg_contents empty table if needed
+  if (!"gpkg_contents" %in% gpkg_list_tables(x)){
+    x <- gpkg_create_contents(x)
+  }
+  
+  # append to gpkg_contents
+  x <- gpkg_execute(x,
+                    paste0(
+                      "INSERT INTO gpkg_contents (table_name, data_type, identifier, 
+                                  description, last_change,
+                                  min_x, min_y, max_x, max_y, srs_id) 
+       VALUES ('",
+                      table_name ,
+                      "', 'attributes', '",
+                      table_name,
+                      "', '",
+                      description,
+                      "','",
+                      strftime(Sys.time(), '%Y-%m-%dT%H:%M:%OS3Z'),
+                      "', ", ex[1], ", ", ex[2], ", ",
+                             ex[3], ", ", ex[4], ", ", 
+                             cr,"
+                      );"
+                    )
+  )
+  x
+}
+
+#' @description `gpkg_update_contents()`: Add and remove `gpkg_contents` records to match existing tables
+#' @rdname gpkg-contents
 #' @export
 gpkg_update_contents <- function(x) {
   contents <- try(gpkg_contents(x))
   if (inherits(contents, 'try-error')) {
     # create minimal gpkg_contents table
-    .gpkg_create_contents(x)
+    gpkg_create_contents(x)
   }
   tables <- gpkg_list_tables(x)
   tables_nonstandard <- tables[!grepl("^gpkg_.*|rtree_.*", tables)]
   todo <- tables_nonstandard[!tables_nonstandard %in% contents$table_name]
-
+  torem <- contents$table_name[!contents$table_name %in% tables]
+  
   # create gpkg_contents records, 
-  # TODO: set extent (? is this needed for non-features/grids?)
+  # TODO: set extent via template?
   for (y in todo) {
-    x <- gpkg_add_contents(x, y, y)
+    x <- gpkg_add_contents(x, table_name = y, description = y)
   }
+  for (y in torem) {
+    x <- gpkg_delete_contents(x, table_name = y)
+  }
+  
   x
 }
 
-.gpkg_create_contents <- function(x) {
+#' @description `gpkg_delete_contents()`: Delete a record from `gpkg_contents` based on table name
+#' @rdname gpkg-contents
+#' @export
+gpkg_delete_contents <- function(x, table_name) {
+  gpkg_execute(x, paste0("DELETE FROM gpkg_contents WHERE table_name = ", table_name))
+}
+
+#' @description `gpkg_create_contents()`: Create an empty `gpkg_contents` table
+#' @rdname gpkg-contents
+#' @export
+gpkg_create_contents <- function(x) {
   q <- "CREATE TABLE gpkg_contents (
           table_name TEXT NOT NULL PRIMARY KEY,
           data_type TEXT NOT NULL,
@@ -37,58 +117,4 @@ gpkg_update_contents <- function(x) {
           CONSTRAINT fk_gc_r_srs_id FOREIGN KEY (srs_id) REFERENCES gpkg_spatial_ref_sys(srs_id)
         )"
   gpkg_execute(x, q)
-}
-
-#' Add a record to `gpkg_contents`
-#'
-#' @param x A _geopackage_
-#' @param table_name Name of table to add record for in gpkg_contents
-#' @param description Default `""`
-#' @param template Default `NULL` uses global EPSG:4326 with bounds -180,-90:180,90
-#'
-#' @return A _geopackage_
-#' @export
-gpkg_add_contents <- function(x, table_name, description = "", template = NULL) {
-  
-  if (!missing(template) && !is.null(template)) {
-    
-    # convert sf object to SpatVector
-    if (inherits(template, 'sf')) {
-      template <- terra::vect(template)
-    }
-    
-    # template as a list
-    if (is.list(template)) {
-      ex <- template$ext
-      cr <- template$crs
-    }
-    
-    # template as terra object
-    if (inherits(template, c("SpatRaster", "SpatVector", "SpatVectorProxy"))){
-      ex <- as.numeric(terra::ext(template))
-      cr <- as.character(terra::crs(template))
-    }
-  }
-  
-  # append to gpkg_contents
-  # TODO: allow extent/CRS to be set based on `template` object
-  #  for now probably fine to have global extent for attributes tables
-  x <- gpkg_execute(x,
-               paste0(
-                 "INSERT INTO gpkg_contents (table_name, data_type, identifier, 
-                                  description, last_change,
-                                  min_x, min_y, max_x, max_y, srs_id) 
-       VALUES ('",
-                 table_name ,
-                 "', 'attributes', '",
-                 table_name,
-                 "', '",
-                 description,
-                 "','",
-                 strftime(Sys.time(), '%Y-%m-%dT%H:%M:%OS3Z'),
-                 "', -180, -90, 180, 90, 4326
-      );"
-               )
-  )
-  x
 }
